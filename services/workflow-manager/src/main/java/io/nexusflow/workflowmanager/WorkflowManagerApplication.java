4279570b9c5e9c09f367382ce92085b1bfe0b669
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-07-20 22:15:33 +0530

    Run a test application

---
+++ services/workflow-manager/src/main/java/io/nexusflow/workflowmanager/WorkflowManagerApplication.java
@@ -0,0 +1,11 @@
+package io.nexusflow.workflowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowManagerApplication.class, args);
+    }
+}
