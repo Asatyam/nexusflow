9748b971567e758f18d63860d4cad6c877f3bac5
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2025-07-23 19:36:47 +0530

    Add cycle detection for workflow definitions using Kahn's algorithm

--- services/workflow-manager/src/main/java/io/nexusflow/workflowmanager/service/WorkflowDefinitionParser.java
+++ services/workflow-manager/src/main/java/io/nexusflow/workflowmanager/service/WorkflowDefinitionParser.java
@@ -5,12 +5,10 @@ import io.nexusflow.workflowmanager.domain.DefinitionTasks;
 import io.nexusflow.workflowmanager.domain.TaskNode;
 import io.nexusflow.workflowmanager.domain.WorkflowGraph;
 import io.nexusflow.workflowmanager.entity.WorkflowDefinition;
+import io.nexusflow.workflowmanager.exception.InvalidWorkflowDefinitionException;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -18,7 +16,7 @@ import java.util.stream.Collectors;
 public class WorkflowDefinitionParser {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
-    public DefinitionTasks parseWorkflowDefinitionTasks(WorkflowDefinition workflowDefinition) {
+    private DefinitionTasks parseWorkflowDefinitionTasks(WorkflowDefinition workflowDefinition) {
 
 
         DefinitionTasks definitionTasks;
@@ -31,24 +29,65 @@ public class WorkflowDefinitionParser {
         return definitionTasks;
     }
 
-    public Map<String, TaskNode> buildMapFromTasks(DefinitionTasks definitionTasks) {
+    private Map<String, TaskNode> buildMapFromTasks(DefinitionTasks definitionTasks) {
         return definitionTasks.getTasks().stream().collect(Collectors.toMap(TaskNode::getName, Function.identity()));
     }
 
-    public Map<String, List<String>> buildGraphFromTasks(DefinitionTasks definitionTasks) {
+    private Map<String, List<String>> buildGraphFromTasks(DefinitionTasks definitionTasks) {
         List<TaskNode> tasks = definitionTasks.getTasks();
         Map<String, List<String>> graph = new HashMap<>();
         for(TaskNode task : tasks) {
             graph.computeIfAbsent(task.getName(), k -> new ArrayList<>());
             List<String> dependencies = task.getDependsOn();
             for(String dependency: dependencies) {
-                graph.get(dependency).add(task.getName());
+                graph.computeIfAbsent(dependency, k -> new ArrayList<>()).add(task.getName());
             }
         }
         return graph;
     }
 
-    public List<TaskNode> getStartingNodes(DefinitionTasks definitionTasks) {
+    // This method validates the DAG (Directed Acyclic Graph) of the workflow using Kahn's algorithm.
+    public void detectCycle (WorkflowDefinition workflowDefinition) {
+
+        DefinitionTasks definitionTasks = parseWorkflowDefinitionTasks(workflowDefinition);
+        Map<String, List<String>> graph = buildGraphFromTasks(definitionTasks);
+
+        Queue<String> queue = new LinkedList<>();
+        Map<String, Integer> inDegree = new HashMap<>();
+
+        long visited = 0;
+
+        for(String node : graph.keySet()) {
+            inDegree.put(node, 0);
+        }
+        for(Map.Entry<String, List<String>> entry : graph.entrySet()) {
+            String node = entry.getKey();
+            for(String dependency : entry.getValue()) {
+                inDegree.put(dependency, inDegree.getOrDefault(dependency, 0) + 1);
+            }
+        }
+        for(Map.Entry<String, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.add(entry.getKey());
+            }
+        }
+        while(!queue.isEmpty()) {
+            visited++;
+            String currentNode = queue.poll();
+            List<String> neighbors = graph.get(currentNode);
+            for(String neighbor : neighbors) {
+                inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+                if (inDegree.get(neighbor) == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+        // If there are still nodes with non-zero in-degree, then the graph has a cycle.
+        if(visited != graph.size()) {
+            throw new InvalidWorkflowDefinitionException("Workflow definition contains a cycle");
+        }
+    }
+    private List<TaskNode> getStartingNodes(DefinitionTasks definitionTasks) {
 
         List<TaskNode> startingNodes = new ArrayList<>();
         for(TaskNode task : definitionTasks.getTasks()) {
